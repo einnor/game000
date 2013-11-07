6134a644f63115e2d2539d9eaef2c2feb6ab922b
Ronnie Nyaga <rnafundi@gmail.com>
2013-11-08 01:03:09 +0300

    Added Physics properties to the components in the game

--- src/com/einnor/game000/GameActivity.java
+++ src/com/einnor/game000/GameActivity.java
@@ -4,14 +4,25 @@ import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
+import org.andengine.util.color.Color;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -19,10 +30,11 @@ import android.view.Menu;
 public class GameActivity extends BaseGameActivity {
 
 	Scene scene;
-	protected static final int CAMERA_WIDTH = 720;
+	protected static final int CAMERA_WIDTH = 800;
 	protected static final int CAMERA_HEIGHT = 480;
 	BitmapTextureAtlas playerTexture;
 	ITextureRegion playerTextureRegion;
+	PhysicsWorld physicsWorld;
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
@@ -64,10 +76,26 @@ public class GameActivity extends BaseGameActivity {
 		// TODO Auto-generated method stub
 		this.scene = new Scene();
 		this.scene.setBackground(new Background(0, 125, 58));
+		physicsWorld = new PhysicsWorld(new Vector2(0,
+				SensorManager.GRAVITY_EARTH), false);
+		this.scene.registerUpdateHandler(physicsWorld);
+		createWalls();
 
 		// The callback after the scene has been created
 		pOnCreateSceneCallback.onCreateSceneFinished(this.scene);
 
+	}
+
+	private void createWalls() {
+		// TODO Auto-generated method stub
+		FixtureDef WALLS_FIX = PhysicsFactory
+				.createFixtureDef(0.0f, 0.0f, 0.0f);
+		Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 15, CAMERA_WIDTH,
+				15, this.mEngine.getVertexBufferObjectManager());
+		ground.setColor(new Color(15, 50, 0));
+		PhysicsFactory.createBoxBody(physicsWorld, ground, BodyType.StaticBody,
+				WALLS_FIX);
+		this.scene.attachChild(ground);
 	}
 
 	@Override
@@ -76,9 +104,16 @@ public class GameActivity extends BaseGameActivity {
 		// TODO Auto-generated method stub
 		// Setup a sprite
 		Sprite sPlayer = new Sprite(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 2,
-				playerTextureRegion, this.mEngine.getVertexBufferObjectManager());
+				playerTextureRegion,
+				this.mEngine.getVertexBufferObjectManager());
 		sPlayer.setRotation(45.0f);
+		final FixtureDef PLAYER_FIX = PhysicsFactory.createFixtureDef(10.0f,
+				1.0f, 0.0f);
+		Body body = PhysicsFactory.createCircleBody(physicsWorld, sPlayer,
+				BodyType.DynamicBody, PLAYER_FIX);
 		this.scene.attachChild(sPlayer);
+		physicsWorld.registerPhysicsConnector(new PhysicsConnector(sPlayer,
+				body, true, false));
 
 		// The callback after the scene has been loaded, so as to populate it
 		pOnPopulateSceneCallback.onPopulateSceneFinished();

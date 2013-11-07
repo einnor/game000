1d26fca6d4ed4e445b101b35237087bc66c31251
Ronnie Nyaga <rnafundi@gmail.com>
2013-11-08 00:29:50 +0300

    Created a scene, a camera. Populated the scene with a sprite.

--- src/com/einnor/game000/GameActivity.java
+++ src/com/einnor/game000/GameActivity.java
@@ -1,7 +1,15 @@
 package com.einnor.game000;
 
+import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 
 import android.os.Bundle;
@@ -9,15 +17,22 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class GameActivity extends BaseGameActivity {
-	
+
 	Scene scene;
 	protected static final int CAMERA_WIDTH = 720;
 	protected static final int CAMERA_HEIGHT = 480;
+	BitmapTextureAtlas playerTexture;
+	ITextureRegion playerTextureRegion;
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		// TODO Auto-generated method stub
-		return null;
+		Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		EngineOptions options = new EngineOptions(true,
+				ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(
+						CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
+
+		return options;
 	}
 
 	@Override
@@ -25,11 +40,22 @@ public class GameActivity extends BaseGameActivity {
 			OnCreateResourcesCallback pOnCreateResourcesCallback)
 			throws Exception {
 		// TODO Auto-generated method stub
-		
-		
-		//The callback after loading up all our resources
+		loadGfx();
+
+		// The callback after loading up all our resources
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
-		
+
+	}
+
+	private void loadGfx() {
+		// TODO Auto-generated method stub
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+
+		// Width and height has to be to the power of 2 i.e 2^x
+		playerTexture = new BitmapTextureAtlas(getTextureManager(), 64, 64);
+		playerTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(playerTexture, this, "player.png", 0, 0);
+		playerTexture.load();
 	}
 
 	@Override
@@ -37,21 +63,26 @@ public class GameActivity extends BaseGameActivity {
 			throws Exception {
 		// TODO Auto-generated method stub
 		this.scene = new Scene();
-		
-		//The callback after the scene has been created
+		this.scene.setBackground(new Background(0, 125, 58));
+
+		// The callback after the scene has been created
 		pOnCreateSceneCallback.onCreateSceneFinished(this.scene);
-		
+
 	}
 
 	@Override
 	public void onPopulateScene(Scene pScene,
 			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
 		// TODO Auto-generated method stub
-		
-		
-		//The callback after the scene has been loaded, so as to populate it
+		// Setup a sprite
+		Sprite sPlayer = new Sprite(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 2,
+				playerTextureRegion, this.mEngine.getVertexBufferObjectManager());
+		sPlayer.setRotation(45.0f);
+		this.scene.attachChild(sPlayer);
+
+		// The callback after the scene has been loaded, so as to populate it
 		pOnPopulateSceneCallback.onPopulateSceneFinished();
-		
+
 	}
 
 }
